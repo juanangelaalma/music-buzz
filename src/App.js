807a19fc7bb8aadfad301b664120320c3a930817
juanangelaalma <juanlah77@gmail.com>
2021-08-11 10:03:00 +0700

    create slide wthree

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import About from './components/About'
 import Choose from './components/Choose'
 import Services from './components/Services'
+import Wthree from './components/Wthree'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <About />
       <Choose />
       <Services />
+      <Wthree />
     </Fragment>
   )
 }
---
+++ src/components/Wthree.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Wthree = () => {
+  return (
+    <div className="slide-wthree">
+      <div className="cover">
+        <img src="/i2.png" alt="img-wthree" />
+        <h1>MUSIC BUZZ</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Wthree
